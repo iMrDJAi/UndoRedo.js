daee0d2711aff7ccf6178c576b49cdb11e64fe9d
${Mr.DJA} <42304709+iMrDJAi@users.noreply.github.com>
2020-01-23 21:29:07 +0100

    Update UndoRedo.js

--- src/UndoRedo.js
+++ src/UndoRedo.js
@@ -1,52 +1,67 @@
-function newHistory(cooldownNumber) {
-  if (!cooldownNumber || isNaN(cooldownNumber) || cooldownNumber <= 0) cooldownNumber = 1;
-  var history = {};
-  history.stack = [''];
-  history.currentNumber = 0;
-  history.currentCooldownNumber = 0;
-  history.record = function (data, force) {
-    if (history.currentNumber === history.stack.length - 1) { //checking for regular history updates
-      if ((history.currentCooldownNumber >= cooldownNumber || history.currentCooldownNumber === 0) && force !== true) { //history updates after a new cooldown
-        history.stack.push(data);
-        history.currentNumber++;
-        history.currentCooldownNumber = 1;
-      } else if (history.currentCooldownNumber < cooldownNumber && force !== true) { //history updates during cooldown
-        history.current(data);
-        history.currentCooldownNumber++;
-      } else if (force === true) { //force to record without cooldown
-        history.stack.push(data);
-        history.currentNumber++;
-        history.currentCooldownNumber = cooldownNumber;
-      }
-    } else if (history.currentNumber < history.stack.length - 1) { //checking for history updates after undo
-      if (force !== true) { //history updates after undo
-        history.stack.length = history.currentNumber + 1;
-        history.stack.push(data);
-        history.currentNumber++;
-        history.currentCooldownNumber = 1;
-      } else if (force === true) { ////force to record after undo 
-        history.stack.length = history.currentNumber + 1;
-        history.stack.push(data);
-        history.currentNumber++;
-        history.currentCooldownNumber = cooldownNumber;
+(function () {
+  var UndoRedojs = function (cooldownNumber) {
+    if (!cooldownNumber || isNaN(cooldownNumber) || cooldownNumber <= 0) cooldownNumber = 1;
+    var history = {};
+    history.stack = [''];
+    history.currentNumber = 0;
+    history.currentCooldownNumber = 0;
+    history.record = function (data, force) {
+      if (history.currentNumber === history.stack.length - 1) { //checking for regular history updates
+        if ((history.currentCooldownNumber >= cooldownNumber || history.currentCooldownNumber === 0) && force !== true) { //history updates after a new cooldown
+          history.stack.push(data);
+          history.currentNumber++;
+          history.currentCooldownNumber = 1;
+        } else if (history.currentCooldownNumber < cooldownNumber && force !== true) { //history updates during cooldown
+          history.current(data);
+          history.currentCooldownNumber++;
+        } else if (force === true) { //force to record without cooldown
+          history.stack.push(data);
+          history.currentNumber++;
+          history.currentCooldownNumber = cooldownNumber;
+        }
+      } else if (history.currentNumber < history.stack.length - 1) { //checking for history updates after undo
+        if (force !== true) { //history updates after undo
+          history.stack.length = history.currentNumber + 1;
+          history.stack.push(data);
+          history.currentNumber++;
+          history.currentCooldownNumber = 1;
+        } else if (force === true) { ////force to record after undo 
+          history.stack.length = history.currentNumber + 1;
+          history.stack.push(data);
+          history.currentNumber++;
+          history.currentCooldownNumber = cooldownNumber;
+        }
       }
     }
-  }
-  history.undo = function () {
-    if (history.currentNumber > 0) {
-      history.currentNumber--;
+    history.undo = function (readOnly) {
+      if (history.currentNumber > 0) {
+        if (readOnly !== true) {
+          history.currentNumber--;
+          return history.stack[history.currentNumber];
+        } else {
+          return history.stack[history.currentNumber - 1];
+        }
+      }
+    }
+    history.redo = function (readOnly) {
+      if (history.currentNumber < history.stack.length - 1) {
+        if (readOnly !== true) {
+          history.currentNumber++;
+          return history.stack[history.currentNumber];
+        } else {
+          return history.stack[history.currentNumber + 1];
+        }
+      }
+    }
+    history.current = function (data) {
+      if (data) history.stack[history.currentNumber] = data;
       return history.stack[history.currentNumber];
     }
+    return history;
   }
-  history.redo = function () {
-    if (history.currentNumber < history.stack.length - 1) {
-      history.currentNumber++;
-      return history.stack[history.currentNumber];
-    }
+  if (typeof (module) !== 'undefined' && typeof (module.exports) !== 'undefined') {
+    module.exports = UndoRedojs;
+  } else {
+    window.UndoRedojs = UndoRedojs;
   }
-  history.current = function (data) {
-    if (data) history.stack[history.currentNumber] = data;
-    return history.stack[history.currentNumber];
-  }
-  return history;
-}
+})();
